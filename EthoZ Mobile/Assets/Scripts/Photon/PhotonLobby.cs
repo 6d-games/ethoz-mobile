b0ecce60ff6deba095de168584075e3e5b2d80c2
Zachary Moote <nuluumo@gmail.com>
2019-07-24 19:07:25 -0400

    Updated ChooseYourMode.unity and started writing PhotonLobby.cs

---
+++ EthoZ Mobile/Assets/Scripts/Photon/PhotonLobby.cs
@@ -0,0 +1,69 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+
+    public GameObject battleButton;
+    public GameObject cancelButton;
+
+    private void Awake()
+    {
+        lobby = this; //Creates the singleton, lives within the main menu scene
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(); //Connects to Master photon server
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        battleButton.SetActive(true);
+    }
+
+    public void OnBattleButtonClicked()
+    {
+        battleButton.SetActive(false);
+        PhotonNetwork.JoinRandomRoom();
+        cancelButton.SetActive(true);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random game but failed. There must be no open games available");
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        int randomRoomName = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = 20 };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed, there must already be a room with the same name");
+        CreateRoom();
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("Cancel button has been clicked");
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
