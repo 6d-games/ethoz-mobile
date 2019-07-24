d73b9a76f191f07b7f3171b6625d48fe13da6e60
Zachary Moote <nuluumo@gmail.com>
2019-07-24 15:41:35 -0400

    Lots of stuff imported and added

---
+++ EthoZ Mobile/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+   public void ToChooseYourMode()
+    {
+        SceneManager.LoadScene("ChooseYourMode");
+    }
+
+    public void ToLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void ToOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+
+}
