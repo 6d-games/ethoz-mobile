764b29aa298029ec359de853cbad876336fd5254
Zachary Moote <nuluumo@gmail.com>
2019-07-24 18:14:54 -0400

    Finished Splash Scene

---
+++ EthoZ Mobile/Assets/Scripts/SplashManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        Invoke("TransitionToMainMenu", 3.75f);  
+    }
+
+    public void TransitionToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
