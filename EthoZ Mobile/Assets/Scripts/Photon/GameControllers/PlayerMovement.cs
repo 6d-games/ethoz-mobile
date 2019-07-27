ba587d75ffde702dc069c707f07d54fa71aed570
Zachary Moote <nuluumo@gmail.com>
2019-07-27 13:38:15 -0400

    Updated to Unity 2019.1.12f1

--- EthoZ Mobile/Assets/Scripts/Photon/GameControllers/PlayerMovement.cs
+++ EthoZ Mobile/Assets/Scripts/Photon/GameControllers/PlayerMovement.cs
@@ -1,13 +1,20 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private PhotonView PV;
+    private CharacterController myCC;
+    public float movementSpeed;
+    public float rotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PV = GetComponent<PhotonView>();
+        myCC = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
