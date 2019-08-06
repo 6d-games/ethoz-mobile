c69f64c2e12f89379c20f85b734b55db8f5e6591
Zachary Moote <nuluumo@gmail.com>
2019-08-06 13:02:05 -0400

    Post Processing

--- EthoZ Mobile/Assets/Third-Party Assets/ProceduralToolkit/Scripts/Buildings/Interfaces/IRoofConstructionStrategy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ProceduralToolkit.Buildings
-{
-    public interface IRoofConstructionStrategy
-    {
-        void Construct(IConstructible<MeshDraft> constructible, Transform parentTransform);
-    }
-}
--- EthoZ Mobile/Assets/Third-Party Assets/ProceduralToolkit/Scripts/Buildings/RoofConstructionStrategy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ProceduralToolkit.Buildings
-{
-    public abstract class RoofConstructionStrategy : ScriptableObject, IRoofConstructionStrategy
-    {
-        public abstract void Construct(IConstructible<MeshDraft> constructible, Transform parentTransform);
-    }
-}
